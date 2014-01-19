c085097c55eca78edc6e764abb02837fe75f1873
terrychenism <hahakuku@hotmail.com>
2014-01-18 21:41:54 -0500

    Update switch workspace.py

--- switch workspace.py
+++ switch workspace.py
@@ -2,5 +2,5 @@ import os
 os.chdir('c:\\Users\\Terry\\Desktop\\feedparser')
 print os.getcwd();
 
-进入CMD，然后输入DOS命令进入setup.py文件所在目录，然后输入python setup.py install
-python.exe 加入系统环境变量
+#进入CMD，然后输入DOS命令进入setup.py文件所在目录，然后输入python setup.py install
+#python.exe 加入系统环境变量
