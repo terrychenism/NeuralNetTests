f6bee986e83111f95071de45cb41d68c643b6e15
terrychenism <hahakuku@hotmail.com>
2014-06-01 17:50:30 -0400

    Update csv2mat.m

--- PCANet/csv2mat.m
+++ PCANet/csv2mat.m
@@ -4,9 +4,13 @@ save('testDataK.mat','mnistTestData');
 
 
 %%  
-
-csvread('train.csv',1);
+>> csvread('train.csv',1);
 >> trainData = ans(:,2:end);
 >> save('trainDataK.mat','trainData');
 >> clear
 >> load('trainDataK.mat');
+>> csvread('train.csv',1);
+>> trainLabels = ans(:,1);
+>> save('trainLabelsK.mat','trainLabels');
+>> clear
+>> load('trainLabelsK.mat');
