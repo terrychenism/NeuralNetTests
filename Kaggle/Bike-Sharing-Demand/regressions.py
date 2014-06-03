fb3841da95283476b73acb87e36730920981f98e
terrychenism <hahakuku@hotmail.com>
2014-06-03 15:36:53 -0400

    Create regressions.py

---
+++ Kaggle/Bike-Sharing-Demand/regressions.py
@@ -0,0 +1,68 @@
+"""
+Project: Bike Sharing Demand
+Date: June 3 2014
+"""
+
+dataPath = "/data/"
+outPath = "results/"
+
+import pandas as pd
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+def loadData(datafile):
+    return pd.read_csv(datafile)
+
+def splitDatetime(data):
+    sub = pd.DataFrame(data.datetime.str.split(' ').tolist(), columns = "date time".split())
+    date = pd.DataFrame(sub.date.str.split('-').tolist(), columns="year month day".split())
+    time = pd.DataFrame(sub.time.str.split(':').tolist(), columns = "hour minute second".split())
+    data['year'] = date['year']
+    data['month'] = date['month']
+    data['day'] = date['day']
+    data['hour'] = time['hour'].astype(int)
+    return data
+
+def createDecisionTree():
+    est = DecisionTreeRegressor()
+    return est
+
+def createRandomForest():
+    est = RandomForestRegressor(n_estimators=200)
+    return est
+
+def createExtraTree():
+    est = ExtraTreesRegressor()
+    return est
+
+def predict(est, train, test, features, target):
+
+    est.fit(train[features], train[target])
+
+    with open(outPath + "submission.csv", 'wb') as f:
+        f.write("datetime,count\n")
+
+        for index, value in enumerate(list(est.predict(test[features]))):
+            f.write("%s,%s\n" % (test['datetime'].loc[index], int(value)))
+
+
+def main():
+
+    train = loadData("train.csv")
+    test = loadData("test.csv")
+
+    train = splitDatetime(train)
+    test = splitDatetime(test)
+
+    target = 'count'
+    features = [col for col in train.columns if col not in ['datetime', 'casual', 'registered', 'count']]
+
+    est = createRandomForest()
+    predict(est, train, test, features, target)
+
+
+
+if __name__ == "__main__":
+    main()
