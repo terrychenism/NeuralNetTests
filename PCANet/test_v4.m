cca63e838ab8fc1b8c67a2a75114d40b19c5b6ef
terrychenism <hahakuku@hotmail.com>
2014-06-10 18:46:15 -0400

    cv

--- PCANet/test_v4.m
+++ PCANet/test_v4.m
@@ -1,31 +1,19 @@
-% ==== PCANet Demo =======
-% T.-H. Chan, K. Jia, S. Gao, J. Lu, Z. Zeng, and Y. Ma, 
-% "PCANet: A simple deep learning baseline for image classification?" submitted to IEEE TPAMI. 
-% ArXiv eprint: http://arxiv.org/abs/1404.3606 
-
-% Tsung-Han Chan [thchan@ieee.org]
-% Please email me if you find bugs, or have suggestions or questions!
-% ========================
-
 clear all; close all; clc; 
 addpath('./Utils');
-addpath('./Liblinear');
+% addpath('./Liblinear');
 
-
-TrnSize = 60000; 
+addpath('./liblinear-1.94/matlab');
+TrnSize = 600; 
 ImgSize = 28; 
 ImgFormat = 'gray'; %'color' or 'gray'
 
 %% Loading data from MNIST Basic (10000 training, 2000 validation, 50000 testing) 
 % load mnist_basic data
-load('./MNISTdata/mnist_basic'); 
+% load('./MNISTdata/mnist_basic'); 
 load('trainLabelsK.mat');
 load('trainDataK.mat');
 load('testDataK.mat');
-% Load MNIST database files
-mnistData   = loadMNISTImages('mnist/train-images-idx3-ubyte');
-mnistLabels = loadMNISTLabels('mnist/train-labels-idx1-ubyte');
- 
+
 % ===== Reshuffle the training data =====
 % Randnidx = randperm(size(mnist_train,1)); 
 % mnist_train = mnist_train(Randnidx,:); 
@@ -36,16 +24,13 @@ mnistLabels = loadMNISTLabels('mnist/train-labels-idx1-ubyte');
 % ValData = mnist_train(TrnSize+1:end,1:end-1)';
 % ValLabels = mnist_train(TrnSize+1:end,end);
 
-TrnData = mnistData( 1:end,1:TrnSize);
-TrnLabels = mnistLabels(1:TrnSize);
-% pause;
-
+TrnData = trainData(1:TrnSize, 1:end)';
+TrnLabels = trainLabels(1:TrnSize,end);
 % ValData = trainData(TrnSize+1:TrnSize+2000,1:end)';
 % ValLabel = trainLabels(TrnSize+1:TrnSize+2000, end);
 
 
-% pause;
-clear mnist_train;
+
 
 % TestData = mnist_test(:,1:end-1)';
 % TestLabels = mnist_test(:,end);
@@ -67,7 +52,7 @@ TestLabels = zeros(testlabel,1);
 
 
 clear trainData;
-
+clear mnist_train;
 clear mnist_test;
 clear ans;
 clear mnistData;
@@ -75,19 +60,18 @@ clear mnistLabels;
 
 
 %% ==== Subsampling the Training and Testing sets ============
-% (comment out the following four lines for a complete test) 
-% TrnData = TrnData(:,1:4:end);  % sample around 2500 training samples
-% TrnLabels = TrnLabels(1:4:end); % 
-% 
-% TestData = TestData(:,1:50:end);  % sample around 1000 test samples  
-% TestLabels = TestLabels(1:50:end); 
+%(comment out the following four lines for a complete test) 
+TrnData = TrnData(:,1:4:end);  % sample around 2500 training samples
+TrnLabels = TrnLabels(1:4:end); % 
+
+TestData = TestData(:,1:50:end);  % sample around 1000 test samples  
+TestLabels = TestLabels(1:50:end); 
 
 %% ===========================================================
 
 nTestImg = length(TestLabels);
 
-%% PCANet parameters (they should be funed based on validation set; i.e., ValData & ValLabel)
-% We use the parameters in our IEEE TPAMI submission
+%% PCANet parameters 
 PCANet.NumStages = 2;
 PCANet.PatchSize = 7;
 PCANet.NumFilters = [8 8];
@@ -108,9 +92,31 @@ PCANet_TrnTime = toc;
 clear TrnData_ImgCell; 
 
 
+%% cv
+fprintf(1,'step3: Cross Validation for choosing parameter c...\n');
+% the larger c is, more time should be costed
+c = [2^-6 2^-5 2^-4 2^-3 2^-2 2^-1 2^0 2^1 2^2 2^3];
+max_acc = 0;
+tic;
+for i = 1 : size(c, 2)
+	option = ['-B 1 -c ' num2str(c(i)) ' -v 5 -q -s 0'];
+	fprintf(1,'Stage: %d/%d: c = %d, ', i, size(c, 2), c(i));
+	accuracy =  train(TrnLabels, ftrain',  option);
+	if accuracy > max_acc
+		max_acc = accuracy;
+		best_c = i;
+	end
+end
+fprintf(1,'The best c is c = %d.\n', c(best_c));
+toc;
+
+
+%% train
 fprintf('\n ====== Training Linear SVM Classifier ======= \n')
 tic;
-models = train(TrnLabels, ftrain', '-s 1 -q'); % we use linear SVM classifier (C = 1), calling libsvm library
+option = ['-c' num2str(c(best_c))  '-s 1 -q'];
+%models = train(TrnLabels, ftrain', '-c 0.5 -s 1 -e 0.01 -q'); % we use linear SVM classifier (C = 1), calling libsvm library
+models = train(TrnLabels, ftrain',  option);
 LinearSVM_TrnTime = toc;
 clear ftrain; 
 
