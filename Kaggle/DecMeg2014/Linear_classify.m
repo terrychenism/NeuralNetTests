4ddeb03128c6d39d3c4f659abeedb75701d01cbd
terrychenism <hahakuku@hotmail.com>
2014-06-22 23:13:15 -0400

    Update and rename benchmark_pooling.m to Linear_classify.m

--- Kaggle/DecMeg2014/Linear_classify.m
+++ Kaggle/DecMeg2014/Linear_classify.m
@@ -1,16 +1,7 @@
-% DecMeg2014 example code.
-% Simple prediction of the class labels of the test set by:
-%   - pooling all the triaining trials of all subjects in one dataset.
-%   - Extracting the MEG data in the first 500ms from when the stimulus starts.
-% - Using a linear classifier (elastic net).
-% Implemented by Seyed Mostafa Kia (seyedmostafa.kia@unitn.it) and Emanuele
-% Olivetti (olivetti@fbk.eu) as a benchmark for DecMeg 2014.
 clear all;
-disp('DecMeg2014: https://www.kaggle.com/c/decoding-the-human-brain');
-subjects_train = 1:16;    
+subjects_train = 1:1;    
 disp(strcat('Training on subjects',num2str(subjects_train(1)),':',num2str(subjects_train(end))));
-% We throw away all the MEG data outside the first 0.5sec from when
-% the visual stimulus start:
+
 tmin = 0;
 tmax = 0.5;
 disp(strcat('Restricting MEG data to the interval [',num2str(tmin),num2str(tmax),'] sec.'));
@@ -18,10 +9,11 @@ X_train = [];
 y_train = [];
 X_test = [];
 ids_test = [];
-% Crating the trainset. (Please specify the absolute path for the train data)
+
+
 disp('Creating the trainset.');
 for i = 1 : length(subjects_train)
-    path = 'data/';  % Specify absolute path
+    path = 'data/'; 
     filename = sprintf(strcat(path,'train_subject%02d.mat'),subjects_train(i));
     disp(strcat('Loading ',filename));
     data = load(filename);
@@ -38,12 +30,12 @@ for i = 1 : length(subjects_train)
     X_train = [X_train;features];
     y_train = [y_train;yy];
 end
+pause;
 
-% Crating the testset. (Please specify the absolute path for the test data)
 disp('Creating the testset.');
 subjects_test = 17:23;
 for i = 1 : length(subjects_test)
-    path = 'data/'; % Specify absolute path
+    path = 'data/'; 
     filename = sprintf(strcat(path,'test_subject%02d.mat'),subjects_test(i));
     disp(strcat('Loading ',filename));
     data = load(filename);
@@ -61,11 +53,11 @@ for i = 1 : length(subjects_test)
     ids_test = [ids_test;ids];
 end
 
-% Your training code should be here:
+
 disp('Training the classifier ...')
 [BFinal,FitInfoFinal] = lasso(X_train,single(y_train),'Lambda',0.005,'Alpha',0.9);
 
-% Testing the trained classifier on the test data
+% Testing 
 y_pred = [ones(size(X_test,1),1) X_test] * [FitInfoFinal.Intercept;BFinal];
 y_pred_thresholded = zeros(size(y_pred));
 y_pred_thresholded(y_pred>=median(y_pred))= 1;
