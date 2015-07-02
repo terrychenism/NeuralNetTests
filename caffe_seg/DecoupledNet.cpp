ec2b37205237565002702ddd329696adf74f7bf8
Terry Chen <hahakuku@hotmail.com>
2015-07-01 18:07:10 -0700

    Decoupled net works

--- caffe_seg/DecoupledNet.cpp
+++ caffe_seg/DecoupledNet.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////
 ////// DecoupledNet.cpp
-////// 2015-06-27
+////// 2015-06-23
 ////// Tairui Chen
 ///////////////////////////////////////////////////////////
 
@@ -15,14 +15,16 @@
 #include <opencv/cv.h> //cvResize()
 
 #include "caffe/caffe.hpp"
+#include "caffe/util/math_functions.hpp"
 
 
-
-#define IMAGE_SIZE 224
+#define IMAGE_SIZE 320
 
 using namespace caffe;  // NOLINT(build/namespaces)
 using namespace std;
 using namespace cv;
+
+bool debug_info_ = false;
 
 
 
@@ -112,90 +114,11 @@ void get_top5(float *feature, int arr[5])
 	}
 }
 
-int main(int argc, char** argv)
-{
-	bool debug_info_ = false;
-	if(!debug_info_) caffe::GlobalInit(&argc, &argv);
-
-	vector<vector<float>> pTable = get_pixel( "G:/EDU/_SOURCE_CODE/caffe/caffe-decouple/examples/seg_map/pixel.txt");
-	// Test mode
-	Caffe::set_phase(Caffe::TEST);
-
-	// mode setting - CPU/GPU
-	Caffe::set_mode(Caffe::GPU);
-
-	// gpu device number
-	int device_id = 0;
-	Caffe::SetDevice(device_id);
-
-	// prototxt
-	//Net<float> caffe_test_net("C:/Users/Terry/Desktop/DecoupledNet/model/DecoupledNet_Full_anno/DecoupledNet_Full_anno_inference_deploy.prototxt");
-
-	//// caffemodel
-	//caffe_test_net.CopyTrainedLayersFrom("C:/Users/Terry/Desktop/DecoupledNet/model/DecoupledNet_Full_anno/DecoupledNet_Full_anno_inference.caffemodel");
-
-	Net<float> caffe_test_net("G:/EDU/_SOURCE_CODE/caffe/caffe-decouple/examples/DeconvNet/model/DeconvNet/DeconvNet_inference_deploy.prototxt");
-	// caffemodel
-	//caffe_test_net.CopyTrainedLayersFrom("C:/Users/cht2pal/Desktop/caffe-old-unpool/examples/live_net/VGG_ILSVRC_19_layers.caffemodel");
-	caffe_test_net.CopyTrainedLayersFrom("G:/EDU/_SOURCE_CODE/caffe/caffe-decouple/examples/DeconvNet/model/DeconvNet/DeconvNet_trainval_inference.caffemodel");
-
-
-	//Debug
-	//caffe_test_net.set_debug_info(true);
-
-	clock_t begin = clock();
-  
-	int i=0, j=0, k=0;
-	float mean_val[3] = {104.006, 116.668, 122.678}; // bgr mean
-
-	// input
-	vector<Blob<float>*> input_vec;
-	Blob<float> blob(1, 3, IMAGE_SIZE, IMAGE_SIZE);
-	Blob<float> label_blob(1, 20, 1, 1);
-
-	IplImage *small_image = cvCreateImage(cvSize(IMAGE_SIZE, IMAGE_SIZE), 8, 3);
-
-
-	caffe::Datum datum;
-	const char* img_name = "G:/EDU/_SOURCE_CODE/caffe/caffe-decouple/examples/seg_map/000079.jpg"; //argv[1]; //
-	IplImage *img = cvLoadImage(img_name);
-	cvShowImage("raw_image", img);
-
-	cvResize(img, small_image);
-
-	for (k=0; k<3; k++)
-	{
-		for (i=0; i<IMAGE_SIZE; i++)
-		{
-			for (j=0; j< IMAGE_SIZE; j++)
-			{
-				blob.mutable_cpu_data()[blob.offset(0, k, i, j)] = (float)(unsigned char)small_image->imageData[i*small_image->widthStep+j*small_image->nChannels+k] - mean_val[k];
-			}
-		}
-	}
-	for(int p = 0; p < 20; p++)
-		label_blob.mutable_cpu_data()[label_blob.offset(0,p,0,0)] = 0;
-
-	input_vec.push_back(&blob);
-	//input_vec.push_back(&label_blob);
-
-	float loss;
-
-	// Do Forward
-	const vector<Blob<float>*>& result = caffe_test_net.Forward(input_vec, &loss);
-	LOG(INFO) << "result blobs: " << result.size() << endl;
-	for(auto &out_blob : result){
-		LOG(INFO) << out_blob->num() << " " << out_blob->channels() << " " 
-			<< out_blob->height() << " " << out_blob->width() ;
-	}
-	//Blob<float> *out_blob= result[0];
-
-
-	// Softmax
+vector<Blob<float>*> do_softmax(Blob<float>* result){
 	vector<Blob<float>*> top;
 	vector<Blob<float>*> bottom;
-	top.push_back(result[0]);
-	bottom.push_back(result[0]);
+	top.push_back(result);
+	bottom.push_back(result);
 
 	top[0]->Reshape(bottom[0]->num(), bottom[0]->channels(),
 		bottom[0]->height(), bottom[0]->width());
@@ -227,13 +150,13 @@ int main(int argc, char** argv)
 			}
 		}
 		if(debug_info_) for(int m = 0;m < 10; m++) cout << bottom_data[m] << " " << scale_data[m] <<" " << top_data[m] << endl;
-		
+
 		// subtraction
 		caffe_cpu_gemm<float>(CblasNoTrans, CblasNoTrans, channels, spatial_dim,
 			1, -1., sum_multiplier_.cpu_data(), scale_data, 1., top_data + i * dim);
 
 		if(debug_info_) for(int m = 0;m < 10; m++) cout << bottom_data[m] << " " << scale_data[m] <<" " << top_data[m] << endl;
-		
+
 		// exponentiation
 		caffe_exp<float>(dim, top_data + i * dim, top_data + i * dim);
 
@@ -251,10 +174,97 @@ int main(int argc, char** argv)
 				top_data + top[0]->offset(i, j));
 		}
 	}
+	return top;
+}
+	 
+int main(int argc, char** argv)
+{
 
-	Blob<float> *out_blob= top[0];
+	if(!debug_info_) caffe::GlobalInit(&argc, &argv);
 
-	vector<Mat> maps;
+	vector<vector<float>> pTable = get_pixel( "C:/Users/cht2pal/Desktop/caffe-old-unpool/examples/seg_map/pixel.txt");
+	// Test mode
+	Caffe::set_phase(Caffe::TEST);
+
+	// mode setting - CPU/GPU
+	Caffe::set_mode(Caffe::GPU);
+
+	// gpu device number
+	int device_id = 0;
+	Caffe::SetDevice(device_id);
+
+	// prototxt
+	Net<float> caffe_test_net("C:/Users/cht2pal/Desktop/caffe-old-unpool/examples/DecoupledNet/DecoupledNet_Full_anno_inference_deploy_raw.prototxt");
+
+	// caffemodel
+	caffe_test_net.CopyTrainedLayersFrom("C:/Users/cht2pal/Desktop/caffe-old-unpool/examples/DecoupledNet/DecoupledNet_Full_anno_inference.caffemodel");
+
+	//Debug
+	//caffe_test_net.set_debug_info(true);
+
+	clock_t begin = clock();
+  
+	int i=0, j=0, k=0;
+	float mean_val[3] = {104.006, 116.668, 122.678}; // bgr mean
+
+	// input
+	vector<Blob<float>*> input_vec;
+	Blob<float> blob(1, 3, IMAGE_SIZE, IMAGE_SIZE);
+	Blob<float> label_blob(1, 20, 1, 1);
+
+	IplImage *small_image = cvCreateImage(cvSize(IMAGE_SIZE, IMAGE_SIZE), 8, 3);
+
+
+	caffe::Datum datum;
+	const char* img_name = "C:/Users/cht2pal/Desktop/example/DecoupledNet/inference/data/VOC2012_TEST/JPEGImages/2008_000012.jpg";//argv[1]; //
+	IplImage *img = cvLoadImage(img_name);
+	cvShowImage("raw_image", img);
+
+	cvResize(img, small_image);
+
+	for (k=0; k<3; k++)
+	{
+		for (i=0; i<IMAGE_SIZE; i++)
+		{
+			for (j=0; j< IMAGE_SIZE; j++)
+			{
+				blob.mutable_cpu_data()[blob.offset(0, k, i, j)] = (float)(unsigned char)small_image->imageData[i*small_image->widthStep+j*small_image->nChannels+k] - mean_val[k];
+			}
+		}
+	}
+	input_vec.push_back(&blob);
+
+	float loss;
+
+	// Do Forward
+	const vector<Blob<float>*>& result = caffe_test_net.Forward(input_vec, &loss);
+	if(debug_info_){
+		LOG(INFO) << "result blobs: " << result.size() << endl;
+		for(auto &out_blob : result){
+			LOG(INFO) << out_blob->num() << " " << out_blob->channels() << " " 
+				<< out_blob->height() << " " << out_blob->width() ;
+		}
+	}
+	//Blob<float> *out_blob= result[0];
+	/*ofstream outfile("cnn_output7.txt");
+	for (int ind = 0;ind < result[2]->height() * result[2]->width() ; ind++)
+	outfile <<  result[2]->cpu_data()[ind] << "   ";
+	outfile.close();*/
+
+	for(int p = 0; p < 20; p++){
+		if(result[0]->cpu_data()[i] > 0.5)
+			label_blob.mutable_cpu_data()[label_blob.offset(0,p,0,0)] = 1;
+		else
+			label_blob.mutable_cpu_data()[label_blob.offset(0,p,0,0)] = 0;
+	}
+	input_vec.push_back(&label_blob);
+
+	const vector<Blob<float>*>& result_bk = caffe_test_net.Forward(input_vec, &loss);
+
+	Blob<float> *out_blob= do_softmax(result_bk[2])[0];
+	Blob<float> *cls_score = result_bk[0];
+
+	vector<Mat> score_map;
 	double sum_pix = 0;
 	int idx = 0;
 	int cnt_max = INT_MIN;
@@ -264,45 +274,95 @@ int main(int argc, char** argv)
 	Mat seg_map(IMAGE_SIZE,IMAGE_SIZE, CV_32FC1, Scalar(0,0,0));
 	for(int i = 0; i < out_blob->height(); i++){
 		for(int j=0; j < out_blob->width(); j++){
-			vector<pair<float, int>> v;
-			for(int c = 0; c < out_blob->channels(); c++){ // for 21 channels
-				v.push_back( make_pair(out_blob->cpu_data()[idx + IMAGE_SIZE*IMAGE_SIZE*c], c) );
-			}
-			std::sort(v.begin(), v.end(), [](const std::pair<float, int> &left, const std::pair<float, int> &right) {
-					return left.first >  right.first;} );	
-			//apply pixel
-			seg_map.at<float>(i,j) = v[0].second;
-			//seg_map.at<float>(i,j) = top[0]->cpu_data()[IMAGE_SIZE*IMAGE_SIZE*7 + idx];
+			//vector<pair<float, int>> v;
+			//for(int c = 0; c < out_blob->channels(); c++){ // for 21 channels
+			//	v.push_back( make_pair(out_blob->cpu_data()[idx + IMAGE_SIZE*IMAGE_SIZE*c], c) );
+			//}
+
+			//std::sort(v.begin(), v.end(), [](const std::pair<float, int> &left, const std::pair<float, int> &right) {
+			//		return left.first >  right.first;} );	
+			////apply pixel
+			//seg_map.at<float>(i,j) = v[0].second;
+
+			seg_map.at<float>(i,j) = out_blob->cpu_data()[idx];	
 			idx++;
 		}
 	}
+	score_map.push_back(seg_map);
 	imshow("seg_map", seg_map);
+	seg_map.release();
 
+	for(int i = 0; i < 20; i++){
+		input_vec.clear();
+		Mat seg_map(IMAGE_SIZE,IMAGE_SIZE, CV_32FC1, Scalar(0,0,0));
+		Blob<float> labels(1, 20, 1, 1);
+		for(int p = 0; p < 20; p++)
+			labels.mutable_cpu_data()[labels.offset(0,p,0,0)] = 0;
 
+		if(cls_score->cpu_data()[i] > 0.5){
+			// push data
+			input_vec.push_back(&blob);
+			// push label
+			labels.mutable_cpu_data()[labels.offset(0,i,0,0)] = 1;
+			input_vec.push_back(&labels);
+			float loss_;
+			const vector<Blob<float>*>& cnn_output = caffe_test_net.Forward(input_vec, &loss_);
+			Blob<float> *softmax_score = do_softmax(cnn_output[2])[0];
+
+			int ind = 0;
+			for(int i = 0; i < softmax_score->height(); i++){
+				for(int j = 0; j < softmax_score->width(); j++){
+					seg_map.at<float>(i,j) = softmax_score->cpu_data()[ind+IMAGE_SIZE*IMAGE_SIZE];	
+					ind++;		
+				}
+			}
+
+		}
+		score_map.push_back(seg_map);
+		//imshow("each_channel_map", seg_map);
+		//waitKey();
+		seg_map.release();
+	}
+	cout << score_map.size() << endl;
+
+	Mat res_map(IMAGE_SIZE,IMAGE_SIZE, CV_32FC1, Scalar(0,0,0));
+	for(int i = 0; i < IMAGE_SIZE; i++){
+		for(int j = 0; j < IMAGE_SIZE; j++){
+			vector<pair<float, int>> v;
+			for(int c = 0; c < score_map.size(); c++){ // for 21 channels
+				v.push_back( make_pair(score_map[c].at<float>(i,j), c) );
+			}
+
+			std::sort(v.begin(), v.end(), [](const std::pair<float, int> &left, const std::pair<float, int> &right) {
+					return left.first >  right.first;} );	
+			//apply pixel
+			res_map.at<float>(i,j) = v[0].second;
+		}
+	}
 
 	// restore seg map to image
-	Mat	res_map;
-	resize(seg_map, res_map, Size(img->width, img->height));
+	Mat	cropped_score_map;
+	resize(res_map, cropped_score_map, Size(img->width, img->height));
 	sum_pix = 0;
 	IplImage *final_seg = cvCreateImage(cvSize(img->width, img->height), 8, 3);
-	for(int h = 0; h < res_map.rows; h++){
-		for(int w = 0; w < res_map.cols; w++){
-			int p = res_map.at<float>(h,w);
+	for(int h = 0; h < cropped_score_map.rows; h++){
+		for(int w = 0; w < cropped_score_map.cols; w++){
+			int p = cropped_score_map.at<float>(h,w);
 			for (int c = 0; c < 3; c++){
 				final_seg->imageData[h*img->widthStep+w*img->nChannels+c] = pTable[p][c]*255;
 			}
 		}
 	}
-
+			
 	cvShowImage("Test", final_seg);
 
 	float output[20];
 	double max_prob = INT_MIN;
 	int pred_label;
-	for(int i=0; i<20; i++)
+	for(int i = 0; i < 20; i++)
 	{
 		output[i] = result[0]->cpu_data()[i];
-		cout << output[i] << " " << max_prob;
+		cout << output[i] << " " << max_prob << endl;
 		if(output[i]  > max_prob){
 			max_prob = output[i];	
 			pred_label = i;
