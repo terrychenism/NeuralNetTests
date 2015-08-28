337374c224073cf9b23dc55f282fa46e3141a690
Terry Chen <hahakuku@hotmail.com>
2015-08-28 09:24:22 -0700

    Update demo_parsenet.m

--- caffe_seg/demo_parsenet.m
+++ caffe_seg/demo_parsenet.m
@@ -5,7 +5,7 @@ if exist('../+caffe', 'dir')
 else
   error('Please run this demo from caffe/matlab/demo');
 end
-caffe.reset_all()
+
 load('voc_gt_cmap.mat');
 use_gpu = 1;
 % Set caffe mode
@@ -26,17 +26,37 @@ net = caffe.Net(net_model, net_weights, phase);
 
 
 img = imread('cat.jpg');
-% seg = wl_segmentImage(img);
+end_layer = 'score';
 base_img(:,:,:,1) = permute(img, [2 1 3]);
 octaves = {preprocess(base_img)};
 [H, W, C] = size(base_img);
 blob_index = net.name2blob_index('data');
 net.blob_vec(blob_index).reshape([ H  W 3 1])
 net.blobs('data').set_data(octaves{1}); 
-net.forward_prefilled();
-scores = net.blobs('score').get_data();
-% scores = permute(scores, [2 1 3]);
-[~, seg] = max(scores, [], 3);
+% net.forward_prefilled();
+tic;
+net.forward_to(end_layer);
+toc;
+% scores = net.blobs(end_layer).get_data();
+
+dst = net.blobs(end_layer).get_data();
+net.blobs(end_layer).set_diff(dst);
+% net.backward_prefilled();
+net.backward_fromto('score','pool5');
+g = net.blobs('pool5').get_diff();
+
+% net.backward_from(end_layer);
+% diff = net.blobs('data').get_diff();
+% diff = bsxfun(@minus,diff,min(min(diff)));
+% diff = bsxfun(@rdivide,diff, max(max(diff)));
+% diff_sq = squeeze(diff);
+% [mask,~] = max(diff_sq, [], 4);
+% im_data = permute(mask, [2, 1, 3]);
+% [mask,~] = max(im_data, [], 3);
+% mask = imresize(mask,[H W]);
+% imagesc(mask);
+
+[~, seg] = max(dst, [], 3);
 result_seg = uint8(seg - 1);
 
 % imshow(uint8(seg));
@@ -47,6 +67,7 @@ result_seg_im = reshape(cmap(int32(result_seg)+1,:),[size(result_seg,1),size(res
 result_seg_im = permute(result_seg_im, [2 1 3]);
 imshow(result_seg_im);    
 
+caffe.reset_all();
 end
 
 function crops_data = preprocess(im)
