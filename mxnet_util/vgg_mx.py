7754c4688095f04bcb58100348e77a8bad5492a7
Terry Chen <hahakuku@hotmail.com>
2015-11-14 21:49:42 -0800

    Update vgg_mx.py

--- mxnet_util/vgg_mx.py
+++ mxnet_util/vgg_mx.py
@@ -3,85 +3,59 @@ from data import ilsvrc12_iterator
 import mxnet as mx
 import logging
 workspace_default = 1024
-input_data = mx.symbol.Variable(name="data")
-# group 1
-conv1_1 = mx.symbol.Convolution(
-    data=input_data, kernel=(7, 7), pad=(3, 3), stride=(2,2), num_filter=64, name="conv1_1")
-relu1_1 = mx.symbol.Activation(data=conv1_1, act_type="relu", name="relu1_1")
-# conv1_2 = mx.symbol.Convolution(
-#     data=relu1_1, kernel=(3, 3), pad=(1, 1), num_filter=64, name="conv1_2")
-# relu1_2 = mx.symbol.Activation(data=conv1_2, act_type="relu", name="relu1_2")
-pool1 = mx.symbol.Pooling(
-    data=relu1_1, pool_type="max", kernel=(2, 2), stride=(2,2), name="pool1")
-# group 2
-conv2_1 = mx.symbol.Convolution(
-    data=pool1, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=128, name="conv2_1")
-relu2_1 = mx.symbol.Activation(data=conv2_1, act_type="relu", name="relu2_1")
-conv2_2 = mx.symbol.Convolution(
-    data=relu2_1, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=128, name="conv2_2")
-relu2_2 = mx.symbol.Activation(data=conv2_2, act_type="relu", name="relu2_2")
-conv2_3 = mx.symbol.Convolution(
-    data=relu2_2, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=128, name="conv2_3")
-relu2_3 = mx.symbol.Activation(data=conv2_3, act_type="relu", name="relu2_3")
-conv2_4 = mx.symbol.Convolution(
-    data=relu2_3, kernel=(1, 1), stride=(1, 1), num_filter=256, name="conv2_4")
-relu2_4 = mx.symbol.Activation(data=conv2_4, act_type="relu", name="relu2_4")
 
-pool2 = mx.symbol.Pooling(
-    data=relu2_4, pool_type="max", kernel=(2, 2), stride=(2,2), name="pool2")
-# group 3
-conv3_1 = mx.symbol.Convolution(
-    data=pool2, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=256, name="conv3_1")
-relu3_1 = mx.symbol.Activation(data=conv3_1, act_type="relu", name="relu3_1")
-conv3_2 = mx.symbol.Convolution(
-    data=relu3_1, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=256, name="conv3_2")
-relu3_2 = mx.symbol.Activation(data=conv3_2, act_type="relu", name="relu3_2")
-conv3_3 = mx.symbol.Convolution(
-    data=relu3_2, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=256, name="conv3_3")
-relu3_3 = mx.symbol.Activation(data=conv3_3, act_type="relu", name="relu3_3")
-conv3_4 = mx.symbol.Convolution(
-    data=relu3_3, kernel=(1, 1), stride=(1, 1), num_filter=512, name="conv3_4")
-relu3_4 = mx.symbol.Activation(data=conv3_4, act_type="relu", name="relu3_4")
-pool3 = mx.symbol.Pooling(
-    data=relu3_4, pool_type="max", kernel=(3, 3), pad=(1, 1), stride=(2,2), name="pool3")
-# group 4
-conv4_1 = mx.symbol.Convolution(
-    data=pool3, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=512, name="conv4_1")
-relu4_1 = mx.symbol.Activation(data=conv4_1, act_type="relu", name="relu4_1")
-conv4_2 = mx.symbol.Convolution(
-    data=relu4_1, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=512, name="conv4_2")
-relu4_2 = mx.symbol.Activation(data=conv4_2, act_type="relu", name="relu4_2")
-conv4_3 = mx.symbol.Convolution(
-    data=relu4_2, kernel=(3, 3), pad=(1, 1), stride=(1, 1), num_filter=512, name="conv4_3")
-relu4_3 = mx.symbol.Activation(data=conv4_3, act_type="relu", name="relu4_3")
-conv4_4 = mx.symbol.Convolution(
-    data=relu4_3, kernel=(1, 1), stride=(1, 1), num_filter=1024, name="conv4_4")
-relu4_4 = mx.symbol.Activation(data=conv4_4, act_type="relu", name="relu4_4")
-pool4 = mx.symbol.Pooling(
-    data=relu4_4, pool_type="max", kernel=(3, 3), pad=(1, 1), stride=(2,2), name="pool4")
-# group 5
-conv5_1 = mx.symbol.Convolution(
-    data=pool4, kernel=(1, 1), stride=(1, 1), num_filter=1024, name="conv5_1")
-relu5_1 = mx.symbol.Activation(data=conv5_1, act_type="relu", name="relu5_1")
-conv5_2 = mx.symbol.Convolution(
-    data=relu5_1, kernel=(1, 1), stride=(1, 1), num_filter=1024, name="conv5_2")
-relu5_2 = mx.symbol.Activation(data=conv5_2, act_type="relu", name="conv1_2")
-# conv5_3 = mx.symbol.Convolution(
-#     data=relu5_2, kernel=(3, 3), pad=(1, 1), num_filter=512, name="conv5_3")
-# relu5_3 = mx.symbol.Activation(data=conv5_3, act_type="relu", name="relu5_3")
-pool5 = mx.symbol.Pooling(
-    data=relu5_2, pool_type="ave", kernel=(7, 7), stride=(1,1), name="pool5")
-drop5 = mx.symbol.Dropout(data=pool5, p=0.5, name="drop5")
-# group 6
-conv6 = mx.symbol.Convolution(
-    data=drop5, kernel=(1, 1), stride=(1, 1), num_filter=401, name="conv6")
-softmax = mx.symbol.SoftmaxOutput(data=conv6, name='softmax')
+def get_vgg16_symbol():
+    data = mx.symbol.Variable(name="data")
 
 
+    conv1 = mx.symbol.Convolution(name='conv1', data=data , num_filter=64, pad=(3,3), kernel=(7,7), stride=(2,2), no_bias=False)
+    relu1 = mx.symbol.Activation(name='relu1', data=conv1 , act_type='relu')
+    pool1 = mx.symbol.Pooling(name='pool1', data=relu1 , pad=(0,0), kernel=(2,2), stride=(2,2), pool_type='max')
+    conv2_1 = mx.symbol.Convolution(name='conv2_1', data=pool1 , num_filter=128, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu2_1 = mx.symbol.Activation(name='relu2_1', data=conv2_1 , act_type='relu')
+    conv2_2 = mx.symbol.Convolution(name='conv2_2', data=relu2_1 , num_filter=128, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu2_2 = mx.symbol.Activation(name='relu2_2', data=conv2_2 , act_type='relu')
+    conv2_3 = mx.symbol.Convolution(name='conv2_3', data=relu2_2 , num_filter=128, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu2_3 = mx.symbol.Activation(name='relu2_3', data=conv2_3 , act_type='relu')
+    conv2_4 = mx.symbol.Convolution(name='conv2_4', data=relu2_3 , num_filter=256, pad=(0,0), kernel=(1,1), stride=(1,1), no_bias=False)
+    relu2_4 = mx.symbol.Activation(name='relu2_4', data=conv2_4 , act_type='relu')
+    pool2 = mx.symbol.Pooling(name='pool2', data=relu2_4 , pad=(0,0), kernel=(2,2), stride=(2,2), pool_type='max')
+    conv3_1 = mx.symbol.Convolution(name='conv3_1', data=pool2 , num_filter=256, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu3_1 = mx.symbol.Activation(name='relu3_1', data=conv3_1 , act_type='relu')
+    conv3_2 = mx.symbol.Convolution(name='conv3_2', data=relu3_1 , num_filter=256, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu3_2 = mx.symbol.Activation(name='relu3_2', data=conv3_2 , act_type='relu')
+    conv3_3 = mx.symbol.Convolution(name='conv3_3', data=relu3_2 , num_filter=256, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu3_3 = mx.symbol.Activation(name='relu3_3', data=conv3_3 , act_type='relu')
+    conv3_4 = mx.symbol.Convolution(name='conv3_4', data=relu3_3 , num_filter=512, pad=(0,0), kernel=(1,1), stride=(1,1), no_bias=False)
+    relu3_4 = mx.symbol.Activation(name='relu3_4', data=conv3_4 , act_type='relu')
+    pool3 = mx.symbol.Pooling(name='pool3', data=relu3_4 , pad=(1,1), kernel=(3,3), stride=(2,2), pool_type='max')
+    conv4_1 = mx.symbol.Convolution(name='conv4_1', data=pool3 , num_filter=512, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu4_1 = mx.symbol.Activation(name='relu4_1', data=conv4_1 , act_type='relu')
+    conv4_2 = mx.symbol.Convolution(name='conv4_2', data=relu4_1 , num_filter=512, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu4_2 = mx.symbol.Activation(name='relu4_2', data=conv4_2 , act_type='relu')
+    conv4_3 = mx.symbol.Convolution(name='conv4_3', data=relu4_2 , num_filter=512, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False)
+    relu4_3 = mx.symbol.Activation(name='relu4_3', data=conv4_3 , act_type='relu')
+    conv4_4 = mx.symbol.Convolution(name='conv4_4', data=relu4_3 , num_filter=1024, pad=(0,0), kernel=(1,1), stride=(1,1), no_bias=False)
+    relu4_4 = mx.symbol.Activation(name='relu4_4', data=conv4_4 , act_type='relu')
+    pool4 = mx.symbol.Pooling(name='pool4', data=relu4_4 , pad=(1,1), kernel=(3,3), stride=(2,2), pool_type='max')
+    conv5_1 = mx.symbol.Convolution(name='conv5_1', data=pool4 , num_filter=1024, pad=(0,0), kernel=(1,1), stride=(1,1), no_bias=False)
+    relu5_1 = mx.symbol.Activation(name='relu5_1', data=conv5_1 , act_type='relu')
+    conv5_2 = mx.symbol.Convolution(name='conv5_2', data=relu5_1 , num_filter=1024, pad=(0,0), kernel=(1,1), stride=(1,1), no_bias=False)
+    relu5_2 = mx.symbol.Activation(name='relu5_2', data=conv5_2 , act_type='relu')
+    pool5 = mx.symbol.Pooling(name='pool5', data=relu5_2 , pad=(0,0), kernel=(7,7), stride=(1,1), pool_type='avg')
+    drop5 = mx.symbol.Dropout(name='drop5', data=pool5 , p=0.500000)
+    flatten_0=mx.symbol.Flatten(name='flatten_0', data=drop5)
+    fc8 = mx.symbol.FullyConnected(name='fc8', data=flatten_0 , num_hidden=401, no_bias=False)
+    softmax = mx.symbol.SoftmaxOutput(data=fc8, name='softmax')
+    return softmax
+
+softmax = get_vgg16_symbol()  
+#softmax.save('model.json')
+#print type(softmax)
 ## data
-batch_size = 16
+batch_size = 64
 train, val = ilsvrc12_iterator(batch_size=batch_size, input_shape=(3,224,224))
-
+model_prefix = "model/VGG"
 ## train
 num_gpus = 1
 gpus = [mx.gpu(i) for i in range(num_gpus)]
@@ -92,6 +66,9 @@ model = mx.model.FeedForward(
     learning_rate = 0.01,
     momentum      = 0.9,
     wd            = 0.00001)
+
 logging.basicConfig(level = logging.DEBUG)
 model.fit(X = train, eval_data = val,
-          batch_end_callback = mx.callback.Speedometer(batch_size=batch_size))
+          eval_metric="acc",
+          batch_end_callback=[mx.callback.Speedometer(batch_size), mx.callback.log_train_metric(100)],
+          epoch_end_callback=mx.callback.do_checkpoint(model_prefix))
